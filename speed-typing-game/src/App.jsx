1268d470373e76035311a372ec559932985225e4
abiton198 <abitonp@gmail.com>
2022-09-30 05:23:49 +0200

    .

--- speed-typing-game/src/App.jsx
+++ speed-typing-game/src/App.jsx
@@ -4,50 +4,58 @@ import './App'
 
 function App(){
 
-  const [text, setText] = useState('')
-  const [timeRemaining, setTimeRemaining] = useState(5)
-  const [isTimeRunning, setIsTimeRunning] = useState(false)
+  const STARTING_TIME = 5
 
+  const [text, setText] = useState('')
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME)
+  const [isTimeRunning, setIsTimeRunning] = useState(false)
+  const [wordCount, setWordCount] = useState(0)
   
 
-  function handleChange(event){
-    setText({
-      value: event.target.value
-    })
-  }
-
-  function handleSubmit(event){
-    alert(state.value)
-    event.preventDefault()
+  function handleChange(e){
+    const {value} = e.target
+    setText(value)
   }
 
   function calculateWordCount(text){
-    const wordsArr = text.trim().split('')
-    console.log(wordsArr.length)
-    return words(word => word !== "").length
+    const wordsArr = text.trim().split(" ")
+    return wordsArr.filter(word => word !== "").length
+  }
+
+  function resetGame(){
+    setIsTimeRunning(true)
+    setTimeRemaining(STARTING_TIME)
+    setText('')
+  }
+
+  function endGame(){
+    setIsTimeRunning(false)
+    setWordCount(calculateWordCount(text))
+
   }
 
 useEffect(() => {
-  if(isTimeRunning && timeRemaining === 0) {
+  if(isTimeRunning && timeRemaining > 0) {
     setTimeout(()=> { 
       setTimeRemaining(time => time -1)}, 1000)
     } else if(timeRemaining === 0){
-      setIsTimeRunning(false)
+      endGame()
     }
 },[timeRemaining, isTimeRunning])
 
+const start = isTimeRunning ? "re-START" : "START"
     return (
       <>
-      <form className="App" onSubmit={handleSubmit}>
+      <div className="App">
               <h1>How fast do you type?</h1>
-              <label>
-                <textarea value={text} onChange={handleChange}/>
-              </label>
-              <input type="submit" value="submit"/>
+              <textarea value={text} onChange={handleChange} disabled={!isTimeRunning}/>
+              <button 
+                onClick={resetGame} 
+                disabled={isTimeRunning}
+              >{start}</button>
               <h4>Amount of time remaining:{timeRemaining}</h4>
-              <button onClick={() => setIsTimeRunning(true)} >Start Game</button>
-              <h1>Word Count:</h1>
-      </form>
+              <h1>Word Count:{wordCount}</h1>
+      </div>
       </>
     )
   }
